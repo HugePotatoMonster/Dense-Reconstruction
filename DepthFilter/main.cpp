7255848a3686dd38f62a4d81dc2821d7ccc42e52
GaryDing01 <gding01@qq.com>
2022-05-30 20:18:33 +0800

    Gaussian_Version1

---
+++ DepthFilter/main.cpp
@@ -0,0 +1,209 @@
+/* -*-c++-*- SemiGlobalMatching - Copyright (C) 2020.
+* Author	: Yingsong Li(Ethan Li) <ethan.li.whu@gmail.com>
+* Describe	: main
+*/
+
+#include "stdafx.h"
+#include "SemiGlobalMatching.h"
+#include "DepthFilter.h"
+#include <chrono>
+using namespace std::chrono;
+
+// opencv library
+#include <opencv2/opencv.hpp>
+#ifdef _DEBUG
+#pragma comment(lib,"opencv_world310d.lib")
+#else
+#pragma comment(lib,"opencv_world310.lib")
+#endif
+
+/**
+ * \brief
+ * \param argv 3
+ * \param argc argc[1]:左影像路径 argc[2]: 右影像路径 argc[3]: 最小视差[可选，默认0] argc[4]: 最大视差[可选，默认64]
+ * \param eg. ..\Data\cone\im2.png ..\Data\cone\im6.png 0 64
+ * \param eg. ..\Data\Reindeer\view1.png ..\Data\Reindeer\view5.png 0 128
+ * \return
+ */
+int main(int argv, char** argc)
+{
+    // 求argc长度
+    int len_argc = 0;
+    char* tem = argc[0];
+    while (tem) {
+        len_argc++;
+        tem = argc[len_argc];
+    }
+    printf("The length of argc: %d\n", len_argc);
+    exit(1);
+
+    if (argv < 3) {
+        std::cout << "参数过少，请至少指定左右影像路径！" << std::endl;
+        return -1;
+    }
+
+    //········································································
+    // dch: 初始化深度滤波器
+    DepthFilter::FilterType filter_type = DepthFilter::Gaussion;
+    DepthFilter depth_filter;
+    // depth_filter.Initialize(0.00, 0.00, filter_type);
+
+    //···············································································//
+    // 读取影像
+    std::string path_left = argc[1];
+    std::string path_right = argc[2];
+
+    cv::Mat img_left_c = cv::imread(path_left, cv::IMREAD_COLOR);
+    cv::Mat img_left = cv::imread(path_left, cv::IMREAD_GRAYSCALE);
+    cv::Mat img_right = cv::imread(path_right, cv::IMREAD_GRAYSCALE);
+
+    if (img_left.data == nullptr || img_right.data == nullptr) {
+        std::cout << "读取影像失败！" << std::endl;
+        return -1;
+    }
+    if (img_left.rows != img_right.rows || img_left.cols != img_right.cols) {
+        std::cout << "左右影像尺寸不一致！" << std::endl;
+        return -1;
+    }
+
+
+    //···············································································//
+    const sint32 width = static_cast<uint32>(img_left.cols);
+    const sint32 height = static_cast<uint32>(img_right.rows);
+
+    // 左右影像的灰度数据
+    auto bytes_left = new uint8[width * height];
+    auto bytes_right = new uint8[width * height];
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            bytes_left[i * width + j] = img_left.at<uint8>(i, j);
+            bytes_right[i * width + j] = img_right.at<uint8>(i, j);
+        }
+    }
+
+    printf("Loading Views...Done!\n");
+
+    // SGM匹配参数设计
+    SemiGlobalMatching::SGMOption sgm_option;
+    // 聚合路径数
+    sgm_option.num_paths = 8;
+    // 候选视差范围
+    //sgm_option.min_disparity = argv < 4 ? 0 : atoi(argc[3]);
+    //sgm_option.max_disparity = argv < 5 ? 64 : atoi(argc[4]);
+    // dch: 需要手动设置
+    sgm_option.min_disparity = 0;
+    sgm_option.max_disparity = 64;
+    // census窗口类型
+    sgm_option.census_size = SemiGlobalMatching::Census5x5;
+    // 一致性检查
+    sgm_option.is_check_lr = true;
+    sgm_option.lrcheck_thres = 1.0f;
+    // 唯一性约束
+    sgm_option.is_check_unique = true;
+    sgm_option.uniqueness_ratio = 0.99;
+    // 剔除小连通区
+    sgm_option.is_remove_speckles = true;
+    sgm_option.min_speckle_aera = 50;
+    // 惩罚项P1、P2
+    sgm_option.p1 = 10;
+    sgm_option.p2_init = 150;
+    // 视差图填充
+    // 视差图填充的结果并不可靠，若工程，不建议填充，若科研，则可填充
+    sgm_option.is_fill_holes = false;
+
+    printf("w = %d, h = %d, d = [%d,%d]\n\n", width, height, sgm_option.min_disparity, sgm_option.max_disparity);
+
+    // 定义SGM匹配类实例
+    SemiGlobalMatching sgm;
+
+    //···············································································//
+    // 初始化
+	printf("SGM Initializing...\n");
+    auto start = std::chrono::steady_clock::now();
+    if (!sgm.Initialize(width, height, sgm_option)) {
+        std::cout << "SGM初始化失败！" << std::endl;
+        return -2;
+    }
+    auto end = std::chrono::steady_clock::now();
+    auto tt = duration_cast<std::chrono::milliseconds>(end - start);
+    printf("SGM Initializing Done! Timing : %lf s\n\n", tt.count() / 1000.0);
+
+    //···············································································//
+    // 匹配
+	printf("SGM Matching...\n");
+    start = std::chrono::steady_clock::now();
+    // disparity数组保存子像素的视差结果
+    auto disparity = new float32[uint32(width * height)]();
+    if (!sgm.Match(bytes_left, bytes_right, disparity)) {
+        std::cout << "SGM匹配失败！" << std::endl;
+        return -2;
+    }
+    end = std::chrono::steady_clock::now();
+    tt = duration_cast<std::chrono::milliseconds>(end - start);
+    printf("\nSGM Matching...Done! Timing :   %lf s\n", tt.count() / 1000.0);
+
+    //···············································································//
+	// 显示视差图
+    // 注意，计算点云不能用disp_mat的数据，它是用来显示和保存结果用的。计算点云要用上面的disparity数组里的数据，是子像素浮点数
+    cv::Mat disp_mat = cv::Mat(height, width, CV_8UC1);
+    float min_disp = width, max_disp = -width;
+    for (sint32 i = 0; i < height; i++) {
+        for (sint32 j = 0; j < width; j++) {
+            const float32 disp = disparity[i * width + j];
+            if (disp != Invalid_Float) {
+                min_disp = std::min(min_disp, disp);
+                max_disp = std::max(max_disp, disp);
+            }
+        }
+    }
+    for (sint32 i = 0; i < height; i++) {
+        for (sint32 j = 0; j < width; j++) {
+            const float32 disp = disparity[i * width + j];
+            if (disp == Invalid_Float) {
+                disp_mat.data[i * width + j] = 0;
+            }
+            else {
+                disp_mat.data[i * width + j] = static_cast<uchar>((disp - min_disp) / (max_disp - min_disp) * 255);
+            }
+        }
+    }
+
+    cv::imshow("视差图", disp_mat);
+    cv::Mat disp_color;
+    applyColorMap(disp_mat, disp_color, cv::COLORMAP_JET);
+    cv::imshow("视差图-伪彩", disp_color);
+
+    // 保存结果
+    std::string disp_map_path = argc[1]; disp_map_path += ".1d.png";
+    std::string disp_color_map_path = argc[1]; disp_color_map_path += ".1c.png";
+    cv::imwrite(disp_map_path, disp_mat);
+    cv::imwrite(disp_color_map_path, disp_color);
+
+    // dch:视差图传换成深度图
+    // dch:求出深度对应分布
+    double mu = 1.0;
+    double sigma = 0.0;
+    // 更新深度情况
+    printf("Updating Depth with Depth Dilter...\n");
+
+
+
+    cv::waitKey(0);
+
+    //···············································································//
+    
+    // dch: 释放深度滤波器
+    // 析构函数自己完成
+    
+    // 释放内存
+    delete[] disparity;
+    disparity = nullptr;
+    delete[] bytes_left;
+    bytes_left = nullptr;
+    delete[] bytes_right;
+    bytes_right = nullptr;
+
+    system("pause");
+    return 0;
+}
+
